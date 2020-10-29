a7dbf795342c37f3c63154d29db591c2a910e36a
Bryant <bryantsuiskensmast@gmail.com>
2020-10-29 16:06:45 +0100

    creatie

---
+++ WhizMA/WhizMA/Controllers/ResourcesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WhizMA.Controllers
+{
+    public class ResourcesController : Controller
+    {
+        public IActionResult FAQ()
+        {
+            return View();
+        }
+        public IActionResult Legal()
+        {
+            return View();
+        }
+
+    }
+}
